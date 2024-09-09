d09980f36447f7875ed7a9aa2b8d7ca3f726ba72
KARINA AUDAY <karina.auday@ort.edu.ar>
2024-09-08 21:54:36 -0300

    Creacion de Proyecto

---
+++ 2024-2C-EstacionamientoORT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2024_2C_EstacionamientoORT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
