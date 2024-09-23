dbc215ea0369e5605465a8ab45984352dd91d981
KARINA AUDAY <karina.auday@ort.edu.ar>
2024-09-23 20:37:59 -0300

    Mostrando ejemplos en clase de paso de parametros

--- 2024-2C-EstacionamientoORT/Controllers/HomeController.cs
+++ 2024-2C-EstacionamientoORT/Controllers/HomeController.cs
@@ -18,9 +18,45 @@ namespace _2024_2C_EstacionamientoORT.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Index1(int? num)
         {
-            return View();
+            return View(num);
+        }
+
+
+        public IActionResult Index2()
+        {
+            List<string> ciudades = new List<string> { "Buenos Aires", "Rosario", "Cordoba", "Mendoza" };
+            return View(ciudades);
+        }
+
+
+        public IActionResult ListaNumeros()
+        {
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            return View(numeros);
+        }
+        public IActionResult PasoParametros(int id, string nombreProducto, string descripcion, double precio)
+        {
+            //Chekeo que recibi los parametros
+            if (id > 0 && !String.IsNullOrEmpty(nombreProducto) && !String.IsNullOrEmpty(descripcion))
+            {
+                //Uso ViewBag
+               
+                ViewBag.Id = id;
+                ViewBag.nombreProducto = nombreProducto;
+                //Uso ViewData
+                ViewData["descripcion"] = descripcion;
+                //Uso TempData
+                TempData["Precio"] = precio;
+                return View();
+            }
+            else
+            {
+                //Paso parametros con El New
+
+                return RedirectToAction("Index", "Home", new { mensajeError = "Faltan datos del Producto" });
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
